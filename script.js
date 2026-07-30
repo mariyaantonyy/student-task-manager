a5f65bf68fb5094d7db791d129b0728b9a8a5ff9
Mariya Antony <mariyaanto2005@gmail.com>
2026-07-30 22:13:49 +0530

    First functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let tasks = [];
+let editId = null;
 const task = document.getElementById("task");
 const priority = document.getElementById("priority");
 const dueDate = document.getElementById("dueDate");
@@ -11,20 +13,50 @@ addBtn.addEventListener("click", function(){
         return;
     }
 
-    if(taskList.textContent === "No tasks yet."){
-        taskList.innerHTML = "";
-    }
+    tasks.push({
 
-    taskList.innerHTML += `
-        <div class="task-item">
-            <h3>${task.value}</h3>
-            <p>Priority: ${priority.value}</p>
-            <p>Due: ${dueDate.value}</p>
-        </div>
-    `;
+        id: Date.now(),
+
+        task: task.value,
+
+        priority: priority.value,
+
+        dueDate: dueDate.value,
+
+        completed: false
+
+    });
+
+    displayTasks();
 
     task.value = "";
     priority.selectedIndex = 0;
     dueDate.value = "";
 
 });
+function displayTasks(){
+
+    taskList.innerHTML = "";
+
+    if(tasks.length === 0){
+        taskList.innerHTML = "No tasks yet.";
+        return;
+    }
+
+    tasks.forEach(function(task){
+
+        taskList.innerHTML += `
+        <div class="task-item" data-id="${task.id}">
+
+            <h3>${task.task}</h3>
+
+            <p><strong>Priority:</strong> ${task.priority}</p>
+
+            <p><strong>Due Date:</strong> ${task.dueDate}</p>
+
+        </div>
+        `;
+
+    });
+
+}
